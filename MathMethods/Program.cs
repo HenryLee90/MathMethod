ec55cfaca1ba68c7a9a2de24c814924faa13ab76
Henry Lee <hlee09151@gmail.com>
2022-04-23 13:00:08 -0500

    first init

---
+++ MathMethods/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MathMethods
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jacob");
+            Add(7, 4, 50);
+            Subtract(70, 15);
+            Multiply(6, 82, 5);
+            Console.WriteLine (Divide(50, 5));
+            int answer = Divide(77, 11);
+            Console.WriteLine(answer);
+
+            Console.WriteLine($"Pick A Number Please?.");
+           string userAnswer = Console.ReadLine();
+            Console.WriteLine($"Please Choose Another Number?.");
+            String secUserAnswer = Console.ReadLine();
+            Console.WriteLine($"Please Choose Another Number Again?.");
+            String thirdUserAnswer = Console.ReadLine();
+
+
+
+
+
+        }
+
+        public static void Add(int num1, int num2, int num3 )
+        {
+            int sum = num1 + num2 + num3;
+          Console.WriteLine(sum);            
+
+        }
+        public static void Subtract(int num1, int num2)
+        {
+
+            int sum = num1 - num2;
+            Console.WriteLine(sum);
+
+        }
+        public static void Multiply(int num1, int num2, int num3)
+        {
+            int sum = num1 * num2 * num3;
+            Console.WriteLine(sum);
+
+        }        
+        public static int Divide(int num1, int num2)
+        {
+           int sum = (num1 / num2);
+            
+            return sum;
+
+        }
+    }
+}
